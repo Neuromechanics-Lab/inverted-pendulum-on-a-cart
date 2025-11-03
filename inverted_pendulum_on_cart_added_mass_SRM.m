5ad38032430da9b52f0f178e647f279ad8391414
autumnmroutt <aroutt3@gatech.edu>
2025-11-03 16:29:57 -0500

    Old model, before I made it a function

--- inverted_pendulum_on_cart_added_mass_SRM.m
+++ inverted_pendulum_on_cart_added_mass_SRM.m
@@ -1,3 +1,4 @@
+% OLD AND OUTDATED, USE inverted_pendulum_on_cart_added_mass_SRM_func
 % Autumn Routt
 % 08/27/2025
 % Inverted pendulum on a cart with added mass
@@ -5,23 +6,24 @@
 % T = kp*theta + kv*theta_dot + ka*theta_ddot
 % And common time delay, lambda
 
-clc; clear; 
-% close all;
-
+% clc; clear; 
+% close all; 
+% for m = 0:10:70
 % Defining variables
 M = 73.5; % body mass (kg) (73.5kg = 50th percentile for women in US)
 m = 0; % added mass (kg) (9kg = CDC recommended weight gain for 30 weeks pregant w normal starting BMI)
-h = 1.704; % overall height (m) (1.612m = 50th percentile for women in US)
+h = 1.612; % overall height (m) (1.612m = 50th percentile for women in US)
 l = 0.543*h; % body COM height (m) (avg COM height in women is 0.543*overall height)
-x_a = 0.87; % added mass height (m)
+x_a = l; % added mass height (m)
 y_a = 0.15; % added mass horizontal offset from pendulum arm (m)
+leaningStart = 1; % binary variable to decide if the pendulum will start at a lean to compensate for added mass (1) or at theta zero (0)
 
 theta_a = atan((m*y_a)/(M*l+m*x_a));
 l_lumped = sqrt(((M*l+m*x_a)/(M+m))^2+((m*y_a)/(M+m))^2);
 I_lumped = M*l^2+m*(x_a^2+y_a^2);
 
-kp = 680; % angle gain
-kv = 10; % angular velocity gain
+kp = 500; % angle gain
+kv = 2400; % angular velocity gain
 ka = 0; % angular acceleration gain
 delay = 0; % common time delay (ms), must be <2s and must be an integer
 
@@ -39,13 +41,17 @@ temp_acc((0:pertDuration)+cart_acc_time) = ...
 temp_acc((0:pertDuration)+cart_dec_time) = ...
     cos((0:pertDuration)*2*pi/pertDuration)-1; % deceleration 
 cart_acc_spline = spline(temp_t,temp_acc*50);
-
+dec_end = cart_dec_time+pertDuration;
 
 % use the forward Euler method to find solution with the time delay
 x_sim = zeros(2000,2); % x_sim = [angle, angular velocity]
+if leaningStart==1
+    x_sim(1:2000,1) = -1*theta_a;
+end 
 t_sim = zeros(2000,1);
 ang_acc = zeros(2000,1);
 cartTrq = []; gravTrq = []; muscTrq = [];
+settlingTime = simTime;
 for iter = 2000:2000+size(temp_t,2)
     [dX,cart_trq,gravity_trq,musc_trq] = dPendulumStatesAndTrqs(t_sim, x_sim, ang_acc, cart_acc_spline, M, m, l_lumped, theta_a, I_lumped, kp, kv, ka, iter, delay);
     new_x1 = x_sim(iter,1)+timestep*dX(1,:);
@@ -60,52 +66,77 @@ for iter = 2000:2000+size(temp_t,2)
     cartTrq = [cartTrq;cart_trq];
     gravTrq = [gravTrq;gravity_trq];
     muscTrq = [muscTrq;musc_trq];
+    if settlingTime==simTime && (iter-2000)>dec_end && abs(x_sim(iter,1))<deg2rad(90)
+        recentAngVelVals = x_sim(iter-(0:50),2);
+        angVelNearZero = abs(recentAngVelVals) < 0.02;
+        % slopeAngVel = (x_sim(iter,2)-x_sim(iter-50,2))/(50*timestep);
+        recentAngAccVals = ang_acc(iter-(0:50),1);
+        angAccNearZero = abs(recentAngAccVals) < 0.02;
+        % if sum(angVelNearZero)==51 && slopeAngVel < 0.1
+        if sum(angVelNearZero)==51 && sum(angAccNearZero)==51
+            settlingTime = (iter-2050)*timestep;
+        end 
+    end 
 end 
 x_sim = x_sim(2001:size(x_sim,1),:);
 t_sim = t_sim(2001:size(t_sim,1),:);
 ang_acc = ang_acc(2001:size(ang_acc,1),:);
 
+muscPower = muscTrq.*x_sim(:,2);
+muscGrossWork = trapz(abs(muscPower));
+muscNetWork = trapz(muscPower);
+muscImpulse = trapz(muscTrq);
+muscAvgTrq = mean(abs(muscTrq));
+fprintf('%d kg vals \n', m)
+fprintf('Settling time = %d \n',settlingTime);
+fprintf('Gross work = %d \n',muscGrossWork);
+fprintf('Net work = %d \n',muscNetWork);
+fprintf('Impulse = %d \n',muscImpulse);
+fprintf('Avg torque = %d \n',muscAvgTrq);
 
 % plot result (ang, ang vel, ang acc, trqs)
-% figure
+% fig=figure;
 numplots = 22;
 subplot(numplots,1,1:2)
 plot(temp_t, temp_acc);
-ylabel('cart acceleration')
+ylabel('cart acc')
 titleString = ['k_p=',num2str(kp),', k_v=',num2str(kv),', k_a=',num2str(ka),...
     ', delay=',num2str(delay),', M=',num2str(M),', l=',num2str(l),...
     ', m=',num2str(m),', x_a=',num2str(x_a),', y_a=',num2str(y_a)];
-% title(titleString)
+title(titleString)
 
 subplot(numplots,1,4:7)
-plot(t_sim, x_sim(:,1));
-ylabel('angle (rad)')
+plot(t_sim, rad2deg(x_sim(:,1)));
+ylabel('ang (deg)')
 hold on
 
 subplot(numplots,1,9:12)
-plot(t_sim, x_sim(:,2))
+plot(t_sim, rad2deg(x_sim(:,2)))
 % xlabel('time (s)')
-ylabel('angular velocity (rad/s)')
+ylabel('ang vel (deg/s)')
 hold on
 
 subplot(numplots,1,14:17)
-plot(t_sim,ang_acc(:,1))
+plot(t_sim,rad2deg(ang_acc(:,1)))
 % xlabel('time (s)')
-ylabel('angular acceleration (rad/s^2)')
+ylabel('ang acc (deg/s^2)')
 hold on
 
 subplot(numplots,1,19:22)
 % plot(t_sim,gravTrq)
 % hold on
 % plot(t_sim,cartTrq)
-plot(t_sim,muscTrq)
+plot(t_sim,muscTrq./M)
 xlabel('time (s)')
-ylabel('torque (N*m)')
+ylabel('musc trq (N*m/kg)')
 % legend('Gravity','Cart','Muscles')
 hold on
 
-fileString = ['kp',num2str(kp),'_kv',num2str(kv),'_ka',num2str(ka),'_delay'...
-    ,num2str(delay),'_M',num2str(M),'_l',num2str(l),'_m',num2str(m)...
-    ,'_xa',num2str(x_a),'_ya',num2str(y_a)];
-figName = ['Output/ModelOutput_',fileString,'.fig'];
-savefig(figName)
+% fileString = ['kp',num2str(kp),'_kv',num2str(kv),'_ka',num2str(ka),'_delay'...
+%     ,num2str(delay),'_M',num2str(M),'_l',num2str(l),'_m',num2str(m)...
+%     ,'_xa',num2str(x_a),'_ya',num2str(y_a)];
+% figName = ['Output/ModelOutput_',fileString,'.fig'];
+% savefig(figName)
+% saveas(fig,['Output/ModelOutput_',fileString,'.png']);
+% end
+peakCOMang_noAdapt = max(abs(x_sim(:,1)))
